ee120ed1fc86a0bdfd9df497b33d55aa02a83faa
Rishabh Bohra <rishabh.bohra@bito.ai>
2024-12-03 02:22:13 +0530

    Secret Scanner issue

--- employee_managment/config.js
+++ employee_managment/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    DB_USERNAME: process.env.DB_USERNAME || 'your_mariadb_username',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'your_mariadb_password',
+    DB_USERNAME: process.env.DB_USERNAME || 'employee',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'MAKzQU5nLvTw9n68ETzC',
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_PORT: process.env.DB_PORT || 3306,
     DB_NAME: process.env.DB_NAME || 'employee_mgmt',
