ffb8d376af24fc7a564d1b7a7160e293c62b5750
Rishabh Bohra <rishabh.bohra@bito.ai>
2024-12-11 15:56:02 +0530

    Basic java maven project

---
+++ fbinfer/src/main/java/com/bito/cra/fbinfer/FbinferApplication.java
@@ -0,0 +1,13 @@
+package com.bito.cra.fbinfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbinferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbinferApplication.class, args);
+	}
+
+}
---
+++ fbinfer/src/test/java/com/bito/cra/fbinfer/FbinferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bito.cra.fbinfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbinferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
