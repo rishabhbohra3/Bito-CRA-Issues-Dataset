7005d2591fde5fade0c30f2d00bac2e6e8394a8c
Rishabh Bohra <rishabh.bohra@bito.ai>
2024-12-11 17:29:21 +0530

    fbinfer build 2

---
+++ fbinfer2/src/main/java/com/bito/cra/fbinfer2/Fbinfer2Application.java
@@ -0,0 +1,13 @@
+package com.bito.cra.fbinfer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fbinfer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fbinfer2Application.class, args);
+	}
+
+}
